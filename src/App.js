e4552713069a8ee143d0ce41c61200a8212296cf
Amie Chen <chinghc@gmail.com>
2017-07-18 22:23:22 -0700

    set state from input

--- src/App.js
+++ src/App.js
@@ -10,16 +10,25 @@ class App extends Component {
         {id: 1, name: "Learn Something", isCompleted: true},
         {id: 2, name: "Milk", isCompleted: false},
         {id: 3, name: "Tada", isCompleted: false}
-      ]
+      ],
+      newTodo: ''
     }
+    this.handleInputValue = this.handleInputValue.bind(this)
   }
+
+  handleInputValue(evt) {
+    this.setState({
+      newTodo: evt.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="todo">
           <h2>Todo</h2>
           <form action="">
-            <input type="text"/>
+            <input type="text" value={this.state.newTodo} onChange={this.handleInputValue} />
           </form>
           <ul className="todo-list">
             {this.state.todos.map(todo =>
